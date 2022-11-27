ab00442a774bcdf20e8afca8c4c8719ef335b97b
eoghankbdev <eoghankb@gmail.com>
2022-11-27 14:18:59 +0000

    commented out links to app and js files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <link rel="stylesheet" href="/normal.css"> --}}
+    {{-- <script src="/app.js"></script> --}}
     @vite('resources/css/app.css')
-    <title>Document</title>
+    <title>My Blog</title>
 </head>
 
 <body>
