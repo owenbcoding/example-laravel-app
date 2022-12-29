2e4ca28aa74362cc93c0aba721f5b74917148621
eoghankbdev <eoghankb@gmail.com>
2022-12-29 22:14:58 +0000

    added array map function for file get contents

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\File;
 
 class Post
 {
+    public static function all ()
+    { 
+        $files  = File::files(resource_path("posts/"));
+
+        // Multiple lines in a function 
+        return array_map(function ($file) {
+            return $file->getContents();
+        }, $files);
+
+        //Lambda function single line return statement return array_map(fn($file) => $file->getContents(), $files);
+    }
+
     public static function find($slug)
     {
         base_path();
