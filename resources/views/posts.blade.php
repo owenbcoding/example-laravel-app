e4d316f9f4681596714099d49ca805d982a8e7f9
eoghankbdev <eoghankb@gmail.com>
2022-12-29 22:27:09 +0000

    removed commented out static blog text

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -17,45 +17,6 @@
             <?= $post; ?>
         </article>
     <?php endforeach; ?>
-    {{-- <article class="pb-10 border-b-2 border-slate-400">
-        <h1 class="text-3xl font-bold mb-10 mt-10 text-center">
-            <a href="/posts/my-first-post">First Post!</a>
-        </h1>
-        <p class="leading-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque esse perspiciatis
-            doloribus impedit. Deserunt rerum harum eveniet, molestiae recusandae corrupti cumque nisi voluptatibus
-            voluptatum aliquam soluta autem, exercitationem nobis vel debitis magnam error dignissimos! Sapiente ea iste
-            voluptatum pariatur iure mollitia quo fugiat eveniet deserunt, illo incidunt perferendis vitae natus
-            cupiditate dolores earum eaque numquam culpa omnis. Minus, dolores quidem aut blanditiis eos consequatur
-            aperiam, fugit perspiciatis ab dolorum facilis quae inventore adipisci veritatis asperiores ipsam ipsa
-            aspernatur! Cumque deserunt expedita consequatur dolore molestias sapiente ea nisi minima laudantium fugit
-            accusantium, minus itaque quod earum ratione quis accusamus vitae eius.</p>
-    </article>
-    <article class="pb-10 border-b-2 border-slate-400">
-        <h1 class="text-3xl font-bold mb-10 mt-10 text-center">
-            <a href="/posts/my-second-post">Second Post!</a>
-        </h1>
-        <p class="leading-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque esse perspiciatis
-            doloribus impedit. Deserunt rerum harum eveniet, molestiae recusandae corrupti cumque nisi voluptatibus
-            voluptatum aliquam soluta autem, exercitationem nobis vel debitis magnam error dignissimos! Sapiente ea iste
-            voluptatum pariatur iure mollitia quo fugiat eveniet deserunt, illo incidunt perferendis vitae natus
-            cupiditate dolores earum eaque numquam culpa omnis. Minus, dolores quidem aut blanditiis eos consequatur
-            aperiam, fugit perspiciatis ab dolorum facilis quae inventore adipisci veritatis asperiores ipsam ipsa
-            aspernatur! Cumque deserunt expedita consequatur dolore molestias sapiente ea nisi minima laudantium fugit
-            accusantium, minus itaque quod earum ratione quis accusamus vitae eius.</p>
-    </article>
-    <article class="pb-10 border-b-2 border-slate-400">
-        <h1 class="text-3xl font-bold mb-10 mt-10 text-center">
-            <a href="/posts/my-third-post">Third Post!</a>
-        </h1>
-        <p class="leading-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque esse perspiciatis
-            doloribus impedit. Deserunt rerum harum eveniet, molestiae recusandae corrupti cumque nisi voluptatibus
-            voluptatum aliquam soluta autem, exercitationem nobis vel debitis magnam error dignissimos! Sapiente ea iste
-            voluptatum pariatur iure mollitia quo fugiat eveniet deserunt, illo incidunt perferendis vitae natus
-            cupiditate dolores earum eaque numquam culpa omnis. Minus, dolores quidem aut blanditiis eos consequatur
-            aperiam, fugit perspiciatis ab dolorum facilis quae inventore adipisci veritatis asperiores ipsam ipsa
-            aspernatur! Cumque deserunt expedita consequatur dolore molestias sapiente ea nisi minima laudantium fugit
-            accusantium, minus itaque quod earum ratione quis accusamus vitae eius.</p>
-    </article> --}}
 </body>
 
 </html>
